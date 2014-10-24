288597ed68894d801ba131f3dc5f389e26ff5778
Dmitry Grigoriev <at8eqeq3@yandex.ru>
2014-10-24 11:09:31 +0400

    Now load OAuth keys from environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :flickr, 'yek', 'terces', scope: 'read'
+  provider :flickr, ENV['FLICKR_KEY'], ENV['FLICKR_SECRET'], scope: 'read'
 end
